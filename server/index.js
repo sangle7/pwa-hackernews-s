cde0dc36002887d67d9a2991cd11b5a4b6c1e93a
sangle7 <whb97@163.com>
2017-09-09 22:59:14 +0000

    pwa init

--- server/index.js
+++ server/index.js
@@ -2,19 +2,29 @@ const Koa = require('koa');
 const compress = require('koa-compress')
 const send = require('koa-send');
 const serve = require('koa-static');
+const enforceHttps = require('koa-sslify');
 const app = new Koa();
+
+//app.use(enforceHttps());
+
 // $ GET /hello.txt
-app.use(compress({
+  app.use(compress({
   filter: function (content_type) {
   	return /text|javascript|json/i.test(content_type)
   },
   threshold: 2048,
   flush: require('zlib').Z_SYNC_FLUSH
 }))
-app.use(serve(__dirname+'/../build'));
-app.use(async (ctx) => {
+  app.use(serve('.',{hidden:true}));
+  app.use(serve(__dirname+'/../build'));
+  app.use(async (ctx) => {
   await send(ctx,  '/build/index.html' );
 })
-app.listen(3000);
 
-console.log('listening on port 3000');
+app.listen(8989);
+
+console.log('listening on port 8989');
+
+
+
+
